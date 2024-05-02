25d02d8e3070513a8195f96b19c52f7f5863345c
alexsky1502 <alexsyk1502@gmail.com>
2024-05-02 02:26:18 -0300

    cria exame controller

---
+++ laud-ai/controller/ExameController.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for, redirect
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate  import Migrate
+
+app = Flask(__name__)
+
+###########################
+##### VIEW FUNCTIONS ######
+###########################
+
+@app.route('/')
+def ExameController():
+    pass
