222e26c9fbcf2e8923acb48f8605076b78082c79
Gabriel Novaes <gabrielcarvalho169@hotmail.com>
2024-05-03 16:44:42 -0300

    adiciona arquivos docker locais

--- infra/main.tf
+++ infra/main.tf
@@ -18,49 +18,13 @@ resource "aws_db_instance" "rds" {
   allocated_storage     = 20
   storage_type          = "gp2"
   engine                = "postgres"
-  engine_version        = "12.6"
+  engine_version        = "12"
   instance_class        = "db.t2.micro"
   db_name               = "laudai"
-  username              = "admin"
-  password              = "2engenhariadesoftware!"
-  parameter_group_name  = "default.postgres12"
+  username              = var.db_user
+  password              = var.db_password
+  parameter_group_name  = "default.postgres16"
   publicly_accessible  = false
-
-}
-
-resource "aws_elastic_beanstalk_application" "eb" {
-  name        = "laudai-eb"
-  description = "instancias da aplicacao"
-}
-
-resource "aws_elastic_beanstalk_environment" "eb_env" {
-  name                = "laudai-env"
-  application         = aws_elastic_beanstalk_application.eb.name
-  solution_stack_name = "64bit Amazon Linux 2023 v4.0.11 running Python 3.11"
-
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "VPCId"
-    value     = var.vpc_id
-  }
-
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "Subnets"
-    value     = var.subnets
-  }
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "AssociatePublicIpAddress"
-    value     =  "False"
-  }
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "InstanceType"
-    value     = "t2.micro"
-  }
-
 
 }
 
