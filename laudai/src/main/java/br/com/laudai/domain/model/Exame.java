5d34c3d8c4742fc45744bbbb76ae436bf6db2402
Gabriel Mendes <luizgabrielpm@gmail.com>
2024-05-09 14:44:38 -0300

    Usando Lombok nas classes de domínio

--- laudai/src/main/java/br/com/laudai/domain/model/Exame.java
+++ laudai/src/main/java/br/com/laudai/domain/model/Exame.java
@@ -1,14 +1,18 @@
 package br.com.laudai.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Exame {
-
-    public Exame() {}
 
     public Exame(String nome) {
         this.nome = nome;
@@ -28,35 +32,4 @@ public class Exame {
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Laboratorio> laboratorios;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<Consulta> getConsultas() {
-        return consultas;
-    }
-
-    public void setConsultas(List<Consulta> consultas) {
-        this.consultas = consultas;
-    }
-
-    public List<Laboratorio> getLaboratorios() {
-        return laboratorios;
-    }
-
-    public void setLaboratorios(List<Laboratorio> laboratorios) {
-        this.laboratorios = laboratorios;
-    }
 }
--- laudai/src/main/java/br/com/laudai/domain/model/Laboratorio.java
+++ laudai/src/main/java/br/com/laudai/domain/model/Laboratorio.java
@@ -1,14 +1,18 @@
 package br.com.laudai.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Laboratorio {
-
-    public Laboratorio(){};
 
     public Laboratorio(String nome) {
         this.nome = nome;
@@ -28,35 +32,4 @@ public class Laboratorio {
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Exame> exames;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<Consulta> getConsultas() {
-        return consultas;
-    }
-
-    public void setConsultas(List<Consulta> consultas) {
-        this.consultas = consultas;
-    }
-
-    public List<Exame> getExames() {
-        return exames;
-    }
-
-    public void setExames(List<Exame> exames) {
-        this.exames = exames;
-    }
 }
