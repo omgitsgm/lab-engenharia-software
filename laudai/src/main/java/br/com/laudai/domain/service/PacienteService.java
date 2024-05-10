60e377263d68d73bce8f958fd34321ed9bf174ee
Gabriel Mendes <luizgabrielpm@gmail.com>
2024-05-10 15:26:33 -0300

    Remoção de imports não utilizados

--- laudai/src/main/java/br/com/laudai/domain/service/PacienteService.java
+++ laudai/src/main/java/br/com/laudai/domain/service/PacienteService.java
@@ -1,7 +1,6 @@
 package br.com.laudai.domain.service;
 
 import br.com.laudai.domain.model.Paciente;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
--- laudai/src/main/java/br/com/laudai/infra/repository/LaboratorioRepository.java
+++ laudai/src/main/java/br/com/laudai/infra/repository/LaboratorioRepository.java
@@ -3,9 +3,5 @@ package br.com.laudai.infra.repository;
 import br.com.laudai.domain.model.Laboratorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface LaboratorioRepository extends JpaRepository<Laboratorio, Integer> {
-
-
 }
