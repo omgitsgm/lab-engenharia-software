edd357b5e35bfe774db8c3dd6788238e5489547d
Gabriel Mendes <luizgabrielpm@gmail.com>
2024-05-10 17:54:38 -0300

    Criação do ResponseBody para melhorar o retorn para requisições bem sucedidas.

---
+++ laudai/src/main/java/br/com/laudai/web/http/ResponseBody.java
@@ -0,0 +1,22 @@
+package br.com.laudai.web.http;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ResponseBody {
+
+    private Integer httpStatusCode;
+    private String message;
+    private List<Object> results;
+
+    public ResponseBody(Integer httpStatusCode, String message) {
+        this.httpStatusCode = httpStatusCode;
+        this.message = message;
+    }
+}
