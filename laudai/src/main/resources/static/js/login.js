36eea25b2e8548b6b13aec3eda008f5911fa6172
Gabriel Mendes <luizgabrielpm@gmail.com>
2024-05-15 10:29:52 -0300

    Integração página de login e SessionStorage
    Quando o usuário se autentica, o id dele é armazenado no sessionStorage do navegador, para facilitar a geração das URLs que ele terá acesso na área do paciente na página inicial.

---
+++ laudai/src/main/resources/static/js/login.js
@@ -0,0 +1,48 @@
+const url = "http://localhost:8080";
+const form = document.querySelector('[data-form]');
+const divErros = document.querySelector('[data-erro]');
+
+form.addEventListener("submit", async(e) => {
+    e.preventDefault();
+
+    const dados = {
+        "email": e.target.elements["email"].value,
+        "senha": e.target.elements["senha"].value,
+    }
+
+    let json = JSON.stringify(dados);
+    console.log(json);
+
+    autentica(json);
+
+})
+
+async function autentica(json) {
+
+    const response = await fetch(`${url}/autenticacao`, {
+        method: "POST",
+        body: json,
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    if(!response.ok) {
+        console.log(data.title);
+
+        const paragraphElement = document.createElement("p");
+        paragraphElement.innerText = data.title;
+
+        divErros.innerHTML = "";
+        divErros.appendChild(paragraphElement);
+    } else {
+        // Armazeno o id do usuário no sessionStorage para poder gerar as URL da área do paciente na pagina-inicial
+        sessionStorage.setItem("userId", data.id);
+        window.location.href = `${url}/pagina-inicial`;
+    }
+
+
+}
