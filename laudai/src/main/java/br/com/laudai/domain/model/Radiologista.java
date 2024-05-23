0d35e75a8e6db7f160ca7eb8e2c37582c7275d78
Gabriel Mendes <luizgabrielpm@gmail.com>
2024-05-23 12:37:58 -0300

    Modelagem de Radiologista

---
+++ laudai/src/main/java/br/com/laudai/domain/model/Radiologista.java
@@ -0,0 +1,32 @@
+package br.com.laudai.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Radiologista {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String crm;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @OneToMany
+    private List<Consulta> consultas;
+
+}
